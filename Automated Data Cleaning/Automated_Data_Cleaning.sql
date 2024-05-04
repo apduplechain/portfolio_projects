bf9697f860cc6010eedd400149df7aebb4fb7237
apduplechain <161785365+apduplechain@users.noreply.github.com>
2024-05-03 19:29:39 -0700

    Create Automated_Data_Cleaning.sql

---
+++ Automated Data Cleaning/Automated_Data_Cleaning.sql
@@ -0,0 +1,104 @@
+-- AUTOMATED DATA CLEANING
+
+SELECT *
+FROM us_project.us_household_income_raw
+;
+
+SELECT * FROM us_project.us_household_income_cleaned;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS Copy_and_Clean_Data;
+CREATE PROCEDURE Copy_and_Clean_Data()
+BEGIN
+-- CREATING OUR TABLE
+	CREATE TABLE IF NOT EXISTS `us_household_income_Cleaned` (
+	  `row_id` int DEFAULT NULL,
+	  `id` int DEFAULT NULL,
+	  `State_Code` int DEFAULT NULL,
+	  `State_Name` text,
+	  `State_ab` text,
+	  `County` text,
+	  `City` text,
+	  `Place` text,
+	  `Type` text,
+	  `Primary` text,
+	  `Zip_Code` int DEFAULT NULL,
+	  `Area_Code` int DEFAULT NULL,
+	  `ALand` int DEFAULT NULL,
+	  `AWater` int DEFAULT NULL,
+	  `Lat` double DEFAULT NULL,
+	  `Lon` double DEFAULT NULL,
+	  `TimeStamp` TIMESTAMP DEFAULT NULL
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    
+-- COPY DATA TO NEW TABLE
+	INSERT INTO us_household_income_Cleaned
+    SELECT *, CURRENT_TIMESTAMP
+    FROM us_household_income_raw;
+    
+-- DATA CLEANING STEPS
+	-- 1. REMOVE DUPLICATES
+    DELETE FROM us_household_income_Cleaned
+    WHERE row_id IN (
+			SELECT row_id
+            FROM (
+				SELECT row_id, id,
+                ROW_NUMBER() OVER (PARTITION BY id, `TimeStamp`
+                ORDER BY id, `TimeStamp`) AS row_num
+                FROM us_household_income_Cleaned
+			) duplicates
+			WHERE row_num > 1
+         );
+
+	-- 2. STANDARDIZATION
+    UPDATE us_household_income_Cleaned
+    SET State_Name = 'Georgia'
+    WHERE State_Name = 'georia';
+    
+    UPDATE us_household_income_Cleaned
+    SET County = UPPER(County);
+    
+    UPDATE us_household_income_Cleaned
+    SET City = UPPER(City);
+    
+    UPDATE us_household_income_Cleaned
+    SET Place = UPPER(Place);
+    
+    UPDATE us_household_income_Cleaned
+    SET State_Name = UPPER(State_Name);
+    
+    UPDATE us_household_income_Cleaned
+    SET `Type` = 'CDP'
+    WHERE `Type` = 'CPD';
+    
+    UPDATE us_household_income_Cleaned
+    SET `Type` = 'Borough'
+    WHERE `Type` = 'Boroughs';
+    
+END $$
+DELIMITER ;
+
+CALL Copy_and_Clean_Data();
+
+-- CREATE EVENT
+CREATE EVENT run_date_cleaning
+	ON SCHEDULE EVERY 30 DAY
+    DO CALL Copy_and_Clean_Data();
+
+-- DEBUGGING OR CHECKING SP WORKS
+SELECT row_id, id, row_num
+	FROM (
+		SELECT row_id, id,
+		ROW_NUMBER() OVER (PARTITION BY id
+		ORDER BY id) AS row_num
+		FROM us_household_income_Cleaned
+	) duplicates
+	WHERE row_num > 1
+;
+     
+SELECT COUNT(row_id)
+FROM us_household_income_Cleaned;
+
+SELECT State_Name, COUNT(State_Name)
+FROM us_household_income_Cleaned
+GROUP BY State_Name;
